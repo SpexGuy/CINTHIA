095bebf8a7719a7e86e96c3105c9883af7c8442f
Martin Wickham <spexguy070@gmail.com>
2015-05-10 16:33:11 -0500

    Use G^2 test for speed

--- inference.py
+++ inference.py
@@ -5,13 +5,14 @@ from itertools import combinations
 from itertools import product
 from itertools import chain
 from collections import Counter
+from math import log
 
 # Higher margin => more edges
 INDEPENDENT_MARGIN = 0.01
 
 nameGen = RandomNames()
 
-truth = graph.generateDAG(15, 0.3, nameGen.getRandomName)
+truth = graph.generateDAG(10, 0.3, nameGen.getRandomName)
 #truth = graph.Graph([graph.DirectedNode(n, nameGen.getRandomName()) for n in xrange(5)])
 #truth.addEdge(0,1)
 #truth.addEdge(0,2)
@@ -24,15 +25,18 @@ truth.writeDOT('truth.dot')
 model = truth.copyNodes()
 
 def ICStar(model, truth):
+	print 'Generating data...'
 	controlData = truth.generateDataPoints(100000)
 	controlTable = numpy.ones(tuple(2 for _ in xrange(len(model.getNodes()))), numpy.int32)
 	counts = Counter(tuple(x) for x in controlData)
 	for key in counts:
 		controlTable[key] += counts[key]
-	print controlTable
+	#print controlTable
+	print 'Done!'
+
 	separators = {}
 	for (a, b) in combinations(xrange(len(model.getNodes())), 2):
-		separator = findSeparatingSet(controlData, a, b)
+		separator = findSeparatingSet(controlTable, a, b)
 		if separator is None:
 			model.addEdge(a, b, graph.RELATED_SOMEHOW)
 		else:
@@ -81,36 +85,59 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 
+LN_2 = log(2)
+def log2(x):
+	return log(x)/LN_2
+
+def gSquared(data, varA, varB, conditions):
+	# G = 2 * N * H(a | s) + H(b | s) - H(a,b | s)
+	indexes = sorted((varA, varB) + conditions)
+	indexA = indexes.index(varA)
+	indexB = indexes.index(varB)
+	sumAxes = [x for x in xrange(len(data.shape)) if not x in indexes]
+	#print "    Testing %d and %d given %s, omit %s" % (varA, varB, str(conditions), str(sumAxes))
+	# sum from right to left to avoid moving index problems
+	absData = data
+	for axis in reversed(sumAxes):
+		absData = numpy.sum(absData, axis)
+	asData = numpy.sum(absData, indexB)
+	bsData = numpy.sum(absData, indexA)
+	sData = numpy.sum(asData, indexA)
+	count = numpy.sum(sData)
+#	print absData
+#	print asData
+#	print bsData
+#	print sData
+	h_a_N = 0
+	h_b_N = 0
+	h_ab_N = 0
+	for values in product([0,1], repeat=len(indexes)):
+		sCount = sData[values[:indexA]+values[indexA+1:indexB]+values[indexB+1:]]
+		asCount = asData[values[:indexB]+values[indexB+1:]]
+		bsCount = bsData[values[:indexA]+values[indexA+1:]]
+		absCount = absData[values]
+#		print sCount
+#		print asCount
+#		print bsCount
+#		print absCount
+		if asCount > 0:
+			h_a_N += float(asCount)/count * log2(float(sCount)/asCount)
+		if bsCount > 0:
+			h_b_N += float(bsCount)/count * log2(float(sCount)/bsCount)
+		if absCount > 0:
+			h_ab_N += float(absCount)/count * log2(float(sCount)/absCount)
+
+	return 2 * (h_a_N + h_b_N - h_ab_N)
+		
+
 def independent(data, varA, varB, conditions):
 	# P(varA and varB | conditions) == P(varA | conditions)P(varB | conditions)
 	# TODO: this is really inefficient, and the reason the program is slow
-	conditions = list(conditions)
-	maxDifference = 0
-	for aVal in [0,1]:
-		for bVal in [0,1]:
-			for condVals in product([0,1], repeat=len(conditions)):
-				matchCount = 2
-				aCount = 1
-				bCount = 1
-				bothCount = 1
-				for datum in data:
-					if tuple(datum[conditions]) == condVals:
-						matchCount += 1
-						if datum[varA] == aVal:
-							aCount += 1
-						if datum[varB] == bVal:
-							bCount += 1
-						if datum[varA] == aVal and datum[varB] == bVal:
-							bothCount += 1
-				aProb = float(aCount) / matchCount
-				bProb = float(bCount) / matchCount
-				bothProb = float(bothCount) / matchCount
-				abProb = aProb * bProb
-				difference = abs(bothProb - abProb)
-				maxDifference = max(maxDifference, difference)
-				if difference > INDEPENDENT_MARGIN:
-					return False
-	print "    INDEPENDENT! %s (%f)" % (conditions, maxDifference)
-	return True
+	gsq = gSquared(data, varA, varB, conditions)
+	#print "Gsq(%d %d | %s) = %f" % (varA, varB, str(conditions), gsq)
+	if abs(gsq) < 2.8:
+		print "    INDEPENDENT! %s (%f)" % (conditions, gsq)
+		return True
+	return False
 
 ICStar(model, truth)
