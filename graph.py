db8f025e12b6c61b9dc7b0d385f958ad58a0fcd4
Martin Wickham <spexguy070@gmail.com>
2015-05-10 01:32:12 -0500

    Add graph edge information, types

--- graph.py
+++ graph.py
@@ -1,24 +1,61 @@
 from random import random
 
+# Edge types
+RELATED_SOMEHOW = 0
+LATENT_CERTAIN  = 1
+REVERSE_MAYBE   = 2
+REVERSE_CERTAIN = 3
+FORWARD_MAYBE   = 4
+FORWARD_CERTAIN = 5
+
+UNDIRECTED = 0
+REVERSE = 1
+FORWARD = 2
+
+UNCERTAIN = 0
+CERTAIN = 1
+
+attributes = {
+	FORWARD_CERTAIN: '[dir=forward][label="*"]',
+	FORWARD_MAYBE:   '[dir=forward]',
+	LATENT_CERTAIN:  '[dir=both]',
+	RELATED_SOMEHOW: '[dir=none]',
+	REVERSE_MAYBE:   '[dir=back]',
+	REVERSE_CERTAIN: '[dir=back][label="*"]'
+}
+
+class Edge:
+	def __init__(self, src, dest, edgeType):
+		self._src = src
+		self._dest = dest
+		self._type = edgeType
+		src._addEdge(self)
+		dest._addEdge(self)
+
+	def mayStartWith(self, node):
+		return (node is self._src) if self.isForward() else (node is self._dest) if self.isReverse() else (node is self._src or node is self._dest)
+
+	def isForward(self):
+		return (self._type // 2) == FORWARD
+
+	def isReverse(self):
+		return (self._type // 2) == REVERSE
+
+	def isCertain(self):
+		return (self._type % 2) == CERTAIN
+
+	def getDOTString(self):
+		return '    N%d -> N%d %s\n' % (self._src.getIndex(), self._dest.getIndex(), attributes[self._type])
+
+
 class DirectedNode:
 	def __init__(self, idx, name):
-		self._parents = []
-		self._children = []
+		self._edges = []
 		self._name = name
 		self._idx = idx
 
-	def addParent(self, node):
-		self._parents.append(node)
-
-	def addChild(self, node):
-		self._children.append(node)
-
-	def linkChild(self, node):
-		self.addChild(node)
-		node.addParent(self)
-
-	def getChildren(self):
-		return self._children
+	def _addEdge(self, edge):
+		self._edges.append(edge)
 
 	def getName(self):
 		return self._name
@@ -26,14 +63,21 @@ class DirectedNode:
 	def getIndex(self):
 		return self._idx
 
+
 class Graph:
-	def __init__(self, nodes=None):
-		if (nodes is None):
+	def __init__(self, nodes=None, edges=None):
+		if nodes is None:
 			nodes = []
+		if edges is None:
+			edges = []
 		self._nodes = nodes
+		self._edges = edges
 
 	def getNodes(self):
 		return self._nodes
+
+	def getEdges(self):
+		return self._edges
 
 	def writeDOT(self, filename):
 		dot = open(filename, 'w')
@@ -41,18 +85,18 @@ class Graph:
 		for node in self._nodes:
 			dot.write('    N%d [label="%s"]\n' % (node.getIndex(), node.getName()))
 
-		for node in self._nodes:
-			for child in node.getChildren():
-				dot.write('    N%d -> N%d\n' % (node.getIndex(), child.getIndex()))
+		for edge in self._edges:
+				dot.write(edge.getDOTString())
 
 		dot.write('}\n')
 		dot.close()
 
 def generateDAG(numNodes, edgeProbability, generateName):
 	nodes = [DirectedNode(n, generateName()) for n in xrange(numNodes)]
+	edges = []
 	for i in xrange(numNodes-1):
 		for j in xrange(i+1, numNodes):
 			if (random() < edgeProbability):
-				nodes[i].linkChild(nodes[j])
-	return Graph(nodes)
+				edges.append(Edge(nodes[i], nodes[j], FORWARD_CERTAIN))
+	return Graph(nodes, edges)
 
